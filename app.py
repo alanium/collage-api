416aa2649ff82d6c3af33f128f47856060404a84
Alan <Alan_manuel_gonzalez@hotmail.com>
2024-01-16 15:19:14 -0300

    fc

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-from models.data import db  # Import SQLAlchemy instance
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
@@ -12,12 +12,10 @@ app.config['SECRET_KEY'] = 'AOHDNBIAAI189SD!A1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2305@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from api.routes import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
